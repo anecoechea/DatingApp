92d23eae68263f903f6d54ccef9bc350f598ff57
Andrew Necoechea <andrewnecoechea@gmail.com>
2025-04-08 21:39:55 -0400

    End of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,8 +16,9 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+        return Ok();
 
-        using var hmac = new HMACSHA512();
+        /* using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
@@ -32,7 +33,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user)
-        };
+        }; */
     } 
 
     [HttpPost("login")]
